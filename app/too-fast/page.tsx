b95f52a98de573bd04402054ed9d9b2cbbe4dfb4
PsyKnight <ishanyak12@gmail.com>
2025-01-27 01:43:21 +0530

    Rate limit complete

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,16 @@
+const Page = () => {
+  return (
+    <main className="root-container flex flex-col items-center justify-center min-h-screen">
+      <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
+        Slow down, Blaziken
+      </h1>
+      <p className="mt-3 max-w-xl text-center text-light-400">
+        Looks like you&apos;ve been a little too eager. We&apos;ve put a
+        temporary pause on your excitement. 🚦 Chill for a bit, and try again
+        shortly
+      </p>
+    </main>
+  );
+};
+
+export default Page;
